36729bf87dd8c3bfdda8e1d2275f5e65e74729a9
Wlbrundage47 <55301749+Wlbrundage47@users.noreply.github.com>
2019-11-22 20:22:44 -0500

    Lab 10

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function add() {
+  n1=parseInt(calc.n1.value);
+  n2=parseInt(calc.n2.value);
+  result=n1+n2;
+  calc.result.value=result;
+}
+function subtract() {
+  n1=parseInt(calc.n1.value);
+  n2=parseInt(calc.n2.value);
+  result=n1-n2;
+  calc.result.value=result;
+}
+function devide() {
+  n1=parseInt(calc.n1.value);
+  n2=parseInt(calc.n2.value);
+  result=n1/n2;
+  calc.result.value=result;
+}
+function multiply() {
+  n1=parseInt(calc.n1.value);
+  n2=parseInt(calc.n2.value);
+  result=n1*n2;
+  calc.result.value=result;
+}
